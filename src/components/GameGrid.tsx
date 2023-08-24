6b7e775a7c85ce83459c088a5797958077f0303a
elkrishn <elkrishn@ucsc.edu>
2023-08-24 14:52:29 -0700

    Added icons of the consoles that support each game

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -8,9 +8,13 @@ const GameGrid = () => {
   return (
     <Fragment>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{sm: 1, md: 2, lg: 3, xl: 5}} spacing={10} padding='10px'>
-        {games.map((game)=>(
-            <GameCard key={game.id} game={game} />
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        spacing={10}
+        padding="10px"
+      >
+        {games.map((game) => (
+          <GameCard key={game.id} game={game} />
         ))}
       </SimpleGrid>
     </Fragment>
