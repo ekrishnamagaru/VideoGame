9ea94feea92fc7d2f5a90ffd64b1fb869d089b32
elkrishn <elkrishn@ucsc.edu>
2023-08-24 13:33:15 -0700

    Turned unordered list of game names into grid of cards

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api/",
   params: {
-    key: "",
+    key: "", // left it out for security reasons
   },
 });
