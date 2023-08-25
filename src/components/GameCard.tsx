5928258023d49a38935068b4dfbc88d58e51b4f8
elkrishn <elkrishn@ucsc.edu>
2023-08-25 10:22:02 -0700

    Optimized images in card by making them 600x400

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,13 +2,14 @@ import { Game } from "../hooks/useGames";
 import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import getCroppedImageURL from "../services/image-url";
 interface Props {
   game: Game;
 }
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image} />
+    <Card key={game.id} borderRadius={10} overflow="hidden">
+      <Image src={getCroppedImageURL(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent='space-between'>
